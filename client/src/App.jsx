d6ee8f7a6bf4786abea906c3aa634a78cb7dead4
Tsetan Dorje <tsetangrace@gmail.com>
2023-03-30 19:25:35 +0530

    folder architecture done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,15 +5,38 @@ import {
   Route,
   useLocation
 } from 'react-router-dom';
+import Home from './scenes/home/Home';
+import ItemDetails from './scenes/itemDetails/ItemDetails';
+import Checkout from './scenes/checkout/Checkout';
+import Confirmation from './scenes/checkout/Confirmation';
 
+
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return null;
+}
 
 function App() {
 
   return (
     <div className="app">
-      <h1>Welcome</h1>
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="item/:itemId" element={<ItemDetails />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="checkout/success" element={<Confirmation />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
 
-export default App
+export default App;

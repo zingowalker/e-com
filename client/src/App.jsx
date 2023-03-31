df6fa0c03aefc3a3f1cf9c54ba69bbfb20c9186f
Tsetan Dorje <tsetangrace@gmail.com>
2023-03-31 13:18:20 +0530

    cart menu done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,8 @@ import Home from './scenes/home/Home';
 import ItemDetails from './scenes/itemDetails/ItemDetails';
 import Checkout from './scenes/checkout/Checkout';
 import Confirmation from './scenes/checkout/Confirmation';
+import Navbar from './scenes/global/Navbar';
+import CartMenu from './scenes/global/CartMenu';
 
 
 
@@ -27,6 +29,7 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
+        <Navbar />
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
@@ -34,6 +37,7 @@ function App() {
           <Route path="checkout" element={<Checkout />} />
           <Route path="checkout/success" element={<Confirmation />} />
         </Routes>
+        <CartMenu />
       </BrowserRouter>
     </div>
   )
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,13 +4,23 @@ import App from './App'
 import './index.css'
 import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
-import theme from './theme';
+import { theme } from './theme';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './state';
+
+const store = configureStore({
+  reducer: { cart: cartReducer },
+});
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
 )
